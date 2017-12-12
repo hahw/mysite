8c650bd624f603e0d4e2f35b481b5e7df63299a3
hongweihan <hhw12125@gmail.com>
2017-12-12 10:06:44 +0800

    commit5-7

--- polls/test.py
+++ polls/test.py
@@ -90,3 +90,11 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text='Future question', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
